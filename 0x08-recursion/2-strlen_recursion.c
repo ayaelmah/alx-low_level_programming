fec8efbf041eebfd67b7e1cacc0a25cd31282422
ayaelmah <ayaelmahraoui22@gmail.com>
2024-01-10 14:45:11 -0800

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - main function
+ * @s: function param
+ * Return: return value
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\n')
+	{
+		return (1 + _strlen_recursion(s + 1));
+
+	}
+	return (0);
+}
