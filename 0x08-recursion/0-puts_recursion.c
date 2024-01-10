21a36f70f44d6dcc925db42676d6a58183e5ca4d
ayaelmah <ayaelmahraoui22@gmail.com>
2024-01-10 12:56:49 -0800

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,15 @@
 
 /**
  * puts_recursion - a function that prints a string
- * s: a pointer to a string
+ * @s: a pointer to a string
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	_putchar('\n');
 }
