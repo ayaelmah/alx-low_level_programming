fdf5b066b2489058ef7bdcedde589b7af2c077a4
ayaelmah <ayaelmahraoui22@gmail.com>
2024-01-13 04:36:04 -0800

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a stupid function
+ * @argv: argument vector
+ * @argc: count of arguments
+ * Return: returns 0 if right
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
