3e1272ea7b1bafb6423417b1e50254678ced015b
ayaelmah <ayaelmahraoui22@gmail.com>
2024-01-16 07:14:39 -0800

    task3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - a func that cconcatinates a string.
+ * @s1: string1.
+ * @s2: string2.
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s1len = 0;
+	int s2len = 0;
+	int i;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0 ; s1[i] != '\0' ; i++)
+		s1len++;
+	for (i = 0 ; s2[i] != '\0' ; i++)
+		s2len++;
+	ptr = malloc(sizeof(char) * (s1len + s2len) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0 ; s1[i] != '\0' ; i++)
+		ptr[i] = s1[i];
+	for (i = 0 ; s2[i] != '\0' ; i++)
+		ptr[s1len + 1] = s2[i];
+	return (ptr);
+}
