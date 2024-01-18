5c39c4e7ac094de0d0cbf536692bf3e587c8db51
ayaelmah <ayaelmahraoui22@gmail.com>
2024-01-18 05:10:11 -0800

    3.02

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid - a function with 2 dimensions.
+ *@height: height paramet.
+ *@width: a param.
+ *Return: return 0 if success.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+		free(a);
+	return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			free(a);
+			for (j = 0; j <= height; j++)
+				free(a[j]);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			a[i][j] = 0;
+	}
+	return (a);
+}
