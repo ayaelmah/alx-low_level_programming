178af6970aef1c9a1aea59406b24bfd6097d018d
ayaelmah <ayaelmahraoui22@gmail.com>
2024-01-23 07:41:20 -0800

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structers.
+ * @name: parameetr.
+ * @owner: param2.
+ * @age: an age param
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
