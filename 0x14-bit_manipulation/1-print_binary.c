21836b27cbb409298984bb66aa049af445ba0388
ayaelmah <ayaelmahraoui22@gmail.com>
2024-02-17 14:58:21 -0800

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bit = sizeof(n) * 8, printed = 0;
-
-	while (bit)
+	if (n >> 0)
 	{
-		if (n & 1L << --bit)
-		{
-			_putchar('1');
-			printed++;
-		}
-		else if (printed)
-			_putchar('0');
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	if (!printed)
+	else
+	{
 		_putchar('0');
+	}
 }
